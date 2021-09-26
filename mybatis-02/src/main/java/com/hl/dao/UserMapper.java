8d1eb0e28e2372f16d535fab27deb87aed77e6d4
HeLeiLeiLei <3233583450@qq.com>
2021-09-26 17:06:40 +0800

    优化配置文件和设置别名

--- mybatis-02/src/main/java/com/hl/dao/UserMapper.java
+++ mybatis-02/src/main/java/com/hl/dao/UserMapper.java
@@ -7,7 +7,10 @@ import java.util.Map;
 
 public interface UserMapper {
     List<User> getUserList();
+    //模糊查询
+    List<User> getUserListLike(Map<String, Object> map);
     List<User> getUserListById(int id);
+    List<User> getUserListById2(Map<String, Object> map);
     int addUser(User user);
     int updateUser(User user);
     int deletUser(int id);
